23339ebf3269b1e16a7fb13308885b4bda411980
Lukas Haupt <luhaupt@uni-osnabrueck.de>
2025-08-29 02:08:07 +0200

    feat: add covariance calculation into voxel build

--- include/voxel_downsample.cuh
+++ include/voxel_downsample.cuh
@@ -7,12 +7,14 @@
 #include <vector_types.h>
 
 struct Voxel {
-    float x, y, z;
-    int count;
+    int ix, iy, iz;         // Voxel indices
+    float3 mean;            // Mean, but first: sum of all included points
+    int count;              // Sum of points
+	float covariance[9];    // Covariance, but first: sum of products of points
 };
 
-__global__ void voxelgrid_kernel(const float3* points, int N, float leaf_size, Voxel* voxels, int max_voxels);
-__global__ void compute_centroids(const Voxel* voxels, int max_voxels, float3* centroids, int* amount_centroids);
+__global__ void build_voxels(const float3* points, int N, float leaf_size, Voxel* voxels, int max_voxels);
+__global__ void compute_covariance_and_centroids(Voxel* voxels, int max_voxels, int* amount_centroids);
 
 namespace cuda_vgicp {
     std::vector<float3> voxelgrid_downsample(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, float leaf_size);
--- src/voxel_downsample.cu
+++ src/voxel_downsample.cu
@@ -16,7 +16,7 @@
 #include <iostream>
 #include <vector>
 
-__global__ void voxelgrid_kernel(
+__global__ void build_voxels(
     const float3* points,
     int N,
     float leaf_size,
@@ -24,44 +24,80 @@ __global__ void voxelgrid_kernel(
     int max_voxels
 ) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx >= N) return;
+	if(idx >= N) return;
 
-    float3 p = points[idx];
+	float3 p = points[idx];
     int ix = floorf(p.x / leaf_size);
     int iy = floorf(p.y / leaf_size);
     int iz = floorf(p.z / leaf_size);
     uint32_t hash = ix * 73856093 ^ iy * 19349663 ^ iz * 83492791;
     uint32_t v_idx = hash % max_voxels;
 
-    atomicAdd(&voxels[v_idx].x, p.x);
-    atomicAdd(&voxels[v_idx].y, p.y);
-    atomicAdd(&voxels[v_idx].z, p.z);
-    atomicAdd(&voxels[v_idx].count, 1);
+    // Set voxel indices if not set before
+    if (voxels[v_idx].ix != voxels[v_idx].ix) {
+        voxels[v_idx].ix = ix;
+        voxels[v_idx].iy = iy;
+        voxels[v_idx].iz = iz;
+    }
+
+    // For mean value
+	atomicAdd(&voxels[v_idx].mean.x, p.x);
+	atomicAdd(&voxels[v_idx].mean.y, p.y);
+	atomicAdd(&voxels[v_idx].mean.z, p.z);
+
+	// For covariances
+	atomicAdd(&voxels[v_idx].covariance[0], p.x*p.x);
+	atomicAdd(&voxels[v_idx].covariance[1], p.x*p.y);
+	atomicAdd(&voxels[v_idx].covariance[2], p.x*p.z);
+	atomicAdd(&voxels[v_idx].covariance[3], p.y*p.x);
+	atomicAdd(&voxels[v_idx].covariance[4], p.y*p.y);
+	atomicAdd(&voxels[v_idx].covariance[5], p.y*p.z);
+	atomicAdd(&voxels[v_idx].covariance[6], p.z*p.x);
+	atomicAdd(&voxels[v_idx].covariance[7], p.z*p.y);
+	atomicAdd(&voxels[v_idx].covariance[8], p.z*p.z);
+
+	// For amount of points
+	atomicAdd(&voxels[v_idx].count, 1);
 }
 
-__global__ void compute_centroids(
-    const Voxel* voxels,
+__global__ void compute_covariance_and_centroids(
+    Voxel* voxels,
     int max_voxels,
-    float3* centroids,
     int* amount_centroids
 ) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx >= max_voxels) return;
+	if(idx >= max_voxels) return;
 
-    if(voxels[idx].count > 0) {
-        centroids[idx] = make_float3(
-            voxels[idx].x / voxels[idx].count,
-            voxels[idx].y / voxels[idx].count,
-            voxels[idx].z / voxels[idx].count
-        );
-        atomicAdd(amount_centroids, 1);
-    }
+	Voxel* v = &voxels[idx];
+
+	if(v->count == 0) return;
+
+	// Set real mean value
+	v->mean = make_float3(
+	    v->mean.x / v->count,
+		v->mean.y / v->count,
+		v->mean.z / v->count
+	);
+
+	// Set real covariances
+	v->covariance[0] = v->covariance[0] / v->count - v->mean.x * v->mean.x;
+    v->covariance[1] = v->covariance[1] / v->count - v->mean.x * v->mean.y;
+    v->covariance[2] = v->covariance[2] / v->count - v->mean.x * v->mean.z;
+    v->covariance[3] = v->covariance[3] / v->count - v->mean.y * v->mean.x;
+    v->covariance[4] = v->covariance[4] / v->count - v->mean.y * v->mean.y;
+    v->covariance[5] = v->covariance[5] / v->count - v->mean.y * v->mean.z;
+    v->covariance[6] = v->covariance[6] / v->count - v->mean.z * v->mean.x;
+    v->covariance[7] = v->covariance[7] / v->count - v->mean.z * v->mean.y;
+    v->covariance[8] = v->covariance[8] / v->count - v->mean.z * v->mean.z;
+
+    // Increment amount of centroids if voxel has points
+    atomicAdd(amount_centroids, 1);
 }
 
 std::vector<float3> cuda_vgicp::voxelgrid_downsample(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, float leaf_size) {
     size_t N = cloud->points.size();
     int threadsPerBlock, minBlocksCent;
-    cudaOccupancyMaxPotentialBlockSize(&minBlocksCent, &threadsPerBlock, voxelgrid_kernel, 0, 0);
+    cudaOccupancyMaxPotentialBlockSize(&minBlocksCent, &threadsPerBlock, build_voxels, 0, 0);
     int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
     float3* d_points;
 
@@ -73,8 +109,8 @@ std::vector<float3> cuda_vgicp::voxelgrid_downsample(const pcl::PointCloud<pcl::
     cudaMalloc(&d_voxels, max_voxels * sizeof(Voxel));
     cudaMemset(d_voxels, 0, max_voxels * sizeof(Voxel));
 
-    // Synchronize device after voxelgrid calculation
-    voxelgrid_kernel<<<blocks, threadsPerBlock>>>(d_points, N, leaf_size, d_voxels, max_voxels);
+    // Synchronize device after voxel built
+    build_voxels<<<blocks, threadsPerBlock>>>(d_points, N, leaf_size, d_voxels, max_voxels);
     cudaDeviceSynchronize();
 
     // Synchronize device after centroid calculation
@@ -83,15 +119,17 @@ std::vector<float3> cuda_vgicp::voxelgrid_downsample(const pcl::PointCloud<pcl::
     cudaMalloc(&d_centroids, max_voxels * sizeof(float3));
     cudaMalloc(&d_amount_centroids, sizeof(int));
     cudaMemset(d_amount_centroids, 0, sizeof(int));
-    cudaOccupancyMaxPotentialBlockSize(&minBlocksCent, &threadsPerBlock, compute_centroids, 0, 0);
+    cudaOccupancyMaxPotentialBlockSize(&minBlocksCent, &threadsPerBlock, compute_covariance_and_centroids, 0, 0);
     int blocks_vox = (max_voxels + threadsPerBlock - 1) / threadsPerBlock;
-    compute_centroids<<<blocks_vox, threadsPerBlock>>>(d_voxels, max_voxels, d_centroids, d_amount_centroids);
+    compute_covariance_and_centroids<<<blocks_vox, threadsPerBlock>>>(d_voxels, max_voxels, d_amount_centroids);
     cudaDeviceSynchronize();
 
     int amount_centroids;
     cudaMemcpy(&amount_centroids, d_amount_centroids, sizeof(int), cudaMemcpyDeviceToHost);
     std::vector<float3> downsampled(amount_centroids);
     cudaMemcpy(downsampled.data(), d_centroids, amount_centroids * sizeof(float3), cudaMemcpyDeviceToHost);
+    // std::vector<Voxel> voxels(N);
+    // cudaMemcpy(voxels.data(), d_voxels, N * sizeof(Voxel), cudaMemcpyDeviceToHost);
 
     // Free allocated memory
     cudaFree(d_points);
